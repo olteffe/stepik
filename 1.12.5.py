9b2c7c3bd54bbc3928dfbaa429cbeeb7c78c727c
olteffe <olteffe@gmail.com>
2020-07-20 16:52:04 +0300

    Hint task 1.12.5

--- 1.12.5.py
+++ 1.12.5.py
@@ -1,1 +1,18 @@
-new
+first = int(input())
+second = int(input())
+third = int(input())
+if first >= second and first >= third:
+    if second > third:
+        print(first, third, second, sep="\n")
+    elif second < third:
+        print(first, second, third, sep="\n")
+elif second >= first and second >= third:
+    if first > third:
+        print(second, third, first, sep="\n")
+    elif first < third:
+        print(second, first, third, sep="\n")
+elif third >= first and third >= second:
+    if first > second:
+        print(third, second, first, sep="\n")
+    elif first < second:
+        print(third, first, second, sep="\n")
