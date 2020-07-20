a6b8499667b7967cdda803015e00dc914d5de3f3
olteffe <olteffe@gmail.com>
2020-07-20 17:00:11 +0300

    Small mistake in task 1.12.5

--- 1.12.5.py
+++ 1.12.5.py
@@ -2,17 +2,17 @@ first = int(input())
 second = int(input())
 third = int(input())
 if first >= second and first >= third:
-    if second > third:
+    if second >= third:
         print(first, third, second, sep="\n")
-    elif second < third:
+    elif second <= third:
         print(first, second, third, sep="\n")
 elif second >= first and second >= third:
-    if first > third:
+    if first >= third:
         print(second, third, first, sep="\n")
-    elif first < third:
+    elif first <= third:
         print(second, first, third, sep="\n")
 elif third >= first and third >= second:
-    if first > second:
+    if first >= second:
         print(third, second, first, sep="\n")
-    elif first < second:
+    elif first <= second:
         print(third, first, second, sep="\n")
